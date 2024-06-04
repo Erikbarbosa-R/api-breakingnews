34c6504ca56cd46b1fa4a7eff9f3b850d8e0cbf2
ERIK BARBOSA <erikbarbosa5283@gmail.com>
2024-06-04 10:02:47 -0300

    alteraçoes em

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,20 +2,21 @@ const create = (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background ) {
-        res.status(400).send({message:"Submit all fields for registration"})
+        res.status(400).send({message:"Submit all fields for registration"});
     }
 
-    res..status(201).send({
+    
+    res.status(201).send({
         message: "User created successfully",
         user: {
             name,
             username,
             email,
+            password,
             avatar,
             background,
         },
     });
-
 };
 
 
